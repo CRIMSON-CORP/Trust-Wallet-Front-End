d0ec4b4fb3e4473b5d5e59b52336fbbec55bdc89
CRIMSON <olueatoworosanwo@gmail.com>
2022-02-15 19:44:51 +0100

    Progress at IconButtons, send receive etc

---
+++ screens/Wallet/ActionButtons/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { HStack } from "native-base";
+import IconWrapper from "./IconWrapper/inde";
+
+const ActionButtons = () => {
+    return (
+        <HStack>
+            <IconWrapper text={"Send"} />
+        </HStack>
+    );
+};
+
+export default ActionButtons;
